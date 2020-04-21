eebda49b0046cdfdbeb1562789cef2e42a98ced9
vberger <vberger@users.noreply.github.com>
2020-04-21 09:15:37 +0000

    deploy: 45cf97ef17e09601dc3725ed1e40e6126373e43c

--- source-files.js
+++ source-files.js
@@ -6,5 +6,5 @@ sourcesIndex["wayland_egl"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["wayland_protocols"] = {"name":"","files":["lib.rs","misc.rs","protocol_macro.rs","stable.rs","wlr.rs"]};
 sourcesIndex["wayland_scanner"] = {"name":"","files":["c_code_gen.rs","c_interface_gen.rs","common_gen.rs","lib.rs","parse.rs","protocol.rs","side.rs","util.rs"]};
 sourcesIndex["wayland_server"] = {"name":"","dirs":[{"name":"rust_imp","files":["clients.rs","display.rs","event_loop_glue.rs","globals.rs","mod.rs","resources.rs"]}],"files":["client.rs","display.rs","globals.rs","lib.rs","resource.rs"]};
-sourcesIndex["wayland_sys"] = {"name":"","files":["client.rs","common.rs","cursor.rs","egl.rs","lib.rs","server.rs"]};
+sourcesIndex["wayland_sys"] = {"name":"","files":["client.rs","common.rs","egl.rs","lib.rs","server.rs"]};
 createSourceSidebar();
