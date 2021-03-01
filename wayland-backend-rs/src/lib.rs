cb5ed7f7c45f41cf65cc37a378926b08b6d04f4f
Victor Berger <victor.berger@m4x.org>
2021-03-01 18:22:20 +0100

    backend-rs: client implementation

--- wayland-backend-rs/src/lib.rs
+++ wayland-backend-rs/src/lib.rs
@@ -4,3 +4,12 @@ mod debug;
 mod map;
 mod socket;
 mod wire;
+
+#[inline]
+fn nix_to_io(e: nix::Error) -> std::io::Error {
+    if let nix::Error::Sys(errno) = e {
+        errno.into()
+    } else {
+        panic!("Unexpected nix error: {:?}", e);
+    }
+}
